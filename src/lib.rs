ac3118965fd55293df080d44bee1356373ad25b1
赵光建 <zhaoguangjian@cyberkl.com>
2023-03-20 19:45:18 +0800

    ok

--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,8 @@ fn hide_sha256_in_image(sha256: String) {
     let imgy = 2;
     let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
     let mut chars = sha256.chars();
-    for y in 0..imgy {
-        for x in 0..imgx {
-            // log::info!("{x},{y}");
+    for x in 0..imgx {
+        for y in 0..imgy {
             let pixel = imgbuf.get_pixel_mut(x, y);
             let r = format!("{}{}", chars.next().unwrap(), chars.next().unwrap());
             let g = format!("{}{}", chars.next().unwrap(), chars.next().unwrap());
@@ -29,19 +28,24 @@ fn hide_sha256_in_image(sha256: String) {
             let g = u8::from_str_radix(&g, 16).unwrap();
             let b = u8::from_str_radix(&b, 16).unwrap();
             let a = u8::from_str_radix(&a, 16).unwrap();
-            // log::info!("rgba: {},{},{},{}", r, g, b, a);
             *pixel = image::Rgba([r, g, b, a]);
         }
     }
-    imgbuf.save("./data.png").unwrap();
+    imgbuf.save("./data/data.png").unwrap();
 }
 fn get_sha256_from_image() -> String {
-    let img = image::open("./data.png").unwrap();
+    let img = image::open("./data/data.png").unwrap();
     let mut sha256 = String::new();
-    for (_, _, pixel) in img.to_rgba8().enumerate_pixels() {
-        // log::info!("{x},{y}");
-        let str = format!("{:02x}{:02x}{:02x}{:02x}", pixel[0], pixel[1], pixel[2], pixel[3]);
-        sha256.push_str(&str);
+    let imgbuf = img.to_rgba8();
+    for x in 0..img.width() {
+        for y in 0..img.height() {
+            let pixel = imgbuf.get_pixel(x, y);
+            let str = format!(
+                "{:02x}{:02x}{:02x}{:02x}",
+                pixel[0], pixel[1], pixel[2], pixel[3]
+            );
+            sha256.push_str(&str);
+        }
     }
     sha256
 }
